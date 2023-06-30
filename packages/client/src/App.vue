a055fbc1e9eae4c6c194f8ac9cd7bf753cb6c4d2
CatsJuice <wp1298554944@hotmail.com>
2023-06-30 14:32:38 +0800

    init

---
+++ packages/client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div bg-red bg-green>
+    Set className as attribute at last not working
+  </div>
+
+  <!-- Set className as attribute at last, by without end tag works -->
+  <div bg-gray />
+
+  <!-- eslint-disable-next-line -->
+  <div bg-red bg-blue         >
+    Set className as attribute at last, add space between `attribute` and `>` works
+  </div>
+
+  <div bg-yellow>
+    Set className as attribute at last, className is used in other place, it works
+  </div>
+  <div text-white bg-yellow rounded>
+    2
+  </div>
+
+  <div class="bg-teal">
+    Set className in class attribute, it works
+  </div>
+</template>
---
+++ uno.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  configFile: './projects/app/uno.config.js',
+})
